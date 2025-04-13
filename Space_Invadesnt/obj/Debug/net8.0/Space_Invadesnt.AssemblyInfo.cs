a6699138845821a8833064be0e81d392921a0d58
charlient <carlosjjp2016@gmail.com>
2025-04-12 18:25:01 -0600

    export v0.1 l

--- Space_Invadesnt/obj/Debug/net8.0/Space_Invadesnt.AssemblyInfo.cs
+++ Space_Invadesnt/obj/Debug/net8.0/Space_Invadesnt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Space_Invadesnt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c094e87afe1b20501397fad31f3146c2d6c4b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Space_Invadesnt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Space_Invadesnt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
